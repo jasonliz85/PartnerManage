8d9d51cfd3ac425c03d7dde79eb6b11cc2bf6876
mogoli <cgungaloo@yahoo.com>
2011-01-24 20:02:47 +0000

    My first halfish contribution

--- app/models/work_plan.rb
+++ app/models/work_plan.rb
@@ -17,6 +17,38 @@ class WorkPlan < ActiveRecord::Base
 			self.holiday_booked = self.holidays.count
 		end
 	end
+
+	def yearshiftgen(begindate)
+		noworkplan = self.weekly_rotas.count
+		count = 0
+		begindateweeknumber = begindate.cweek
+		for weeknom in begindateweeknumber..52 do
+				shiftarraygen(self.weekly_rotas[count], weeknom,begindate)
+			count = count + 1
+				if count > (noworkplan -1)
+					count = 0
+				end
+		end
+	end
+
+	def shiftarraygen(weeklyrota, weeknom,begindate)
+
+		shiftentries= Array.new
+		weeklyrota.shift_templates.each_with_index do |shift_template, index|
+			weekdate = DateTime.commercial(begindate.year, weeknom, index+1) - 1.day
+			if shift_template.is_active == true 
+				day = weekdate.day
+				month = weekdate.mon
+				year = weekdate.year
+
+				puts day.to_s + " " + month.to_s + " " + year.to_s
+				shiftstart_at = DateTime.new(year,month,day,shift_template.start_at.hour,shift_template.start_at.min)
+				shiftend_at = DateTime.new(year,month,day,shift_template.end_at.hour, shift_template.end_at.min)
+				shiftentries << {:partner_id => shift_template.weekly_rota.work_plan.partner.id, :name => shift_template.name, :start_at => shiftstart_at,:end_at =>  shiftend_at}
+			end
+		end
+		Shift.create(shiftentries)
+	end
 end
 
 
